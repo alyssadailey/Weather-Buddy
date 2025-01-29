995cdb5d5fd9eed9b869c5b7715fb22c9742277e
alyssadailey <alyssadailey28@gmail.com>
2025-01-28 19:41:21 -0500

    updated weatherRoutes.ts

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,32 +1,54 @@
-import { Router } from 'express';
-import HistoryService from 'server/src/service/historyService.js'
-import WeatherService from 'server/src/service/weatherService.js'
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import { type Request, type Response } from 'express';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
-const router = Router();
+const router = express.Router();
+const weatherService = new WeatherService();
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: GET weather data from city name
   const cityName= req.body.city; 
 
   if (!cityName) {
-    return res.status(400).json({message: 'City name is required'});
+    res.status(400).json({message: 'City name is required'});
+    return;
   }
 
   try {
     // calls weatherService to get weather data
-    const weatherData = await WeatherService.getWeatherByCity(cityName);
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
     
-  }
   // TODO: save city to search history
-  await WeatherService.saveCity(cityName);
+  await HistoryService.addCity(cityName); 
+  res.status(200).json(weatherData);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({error: 'Failed to fetch weather data'});
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {
+interface City {
+  id: string;
+  name: string;
+} 
+interface ErrorResponse{
+  error: string;
+}
 
+router.get('/history', async (req, res): Promise<void> => {
+
+try {
+  const cities: City[] = await HistoryService.getCities();
+ 
+res.status(200).json(cities);
+} catch (error) {
+  console.error('Error fetching search history:', error);
+
+  const errorResponse: ErrorResponse = { error: 'Failed to fetch search history'}
+  res.status(500).json(errorResponse);
+}
 });
 
 // * BONUS TODO: DELETE city from search history
